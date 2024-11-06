dbd5107c83331e1df2f8c6ab141185dd4bac01fc
Aaron Barth <aaronba@users.noreply.github.com>
2024-11-06 12:34:16 -0500

    Update IAuthInfo.cs adding email claim

--- webapi/Auth/IAuthInfo.cs
+++ webapi/Auth/IAuthInfo.cs
@@ -1,16 +1,46 @@
+[EXTERNAL] IAuthInfo.cs
+Barth, Aaron L. (Dell Federal Systems L.p.)<Aaron.Barth@va.gov>
+​
+Aaron Barth
+​
 ﻿// Copyright (c) Microsoft. All rights reserved.
+
+ 
 
 namespace CopilotChat.WebApi.Auth;
 
+ 
+
 public interface IAuthInfo
+
 {
-    /// <summary>
-    /// The authenticated user's unique ID.
-    /// </summary>
-    public string UserId { get; }
 
     /// <summary>
-    /// The authenticated user's name.
+
+    /// The authenticated user's unique ID.
+
     /// </summary>
+
+    public string UserId { get; }
+
+ 
+
+    /// <summary>
+
+    /// The authenticated user's name.
+
+    /// </summary>
+
     public string Name { get; }
+
+ 
+
+    /// <summary>
+
+    /// The authenticated user's email.
+
+    /// </summary>
+
+    public string Email { get; }
+
 }
